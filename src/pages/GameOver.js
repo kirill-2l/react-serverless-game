732e44bb8702275e9ba4e34b7a20314872134df6
Kirill <petrovvkirill@gmail.com>
2020-07-13 17:53:01 +0300

    project connected to netlify

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useScore } from "../contexts/ScoreContext";
+import { StyledLink } from "../styled/Navbar";
 
-const HameOver = () => <h1>HameOver</h1>;
+const HameOver = ({ history }) => {
+  const [score] = useScore();
+  if (score === -1) {
+    history.push("/");
+  }
+  return (
+    <div>
+      <h1>Game Over</h1>
+      <div>{score}</div>
+      <StyledLink to="/">Go Home</StyledLink>
+      <StyledLink to="/game">Play again?</StyledLink>
+    </div>
+  );
+};
 
 export default HameOver;
