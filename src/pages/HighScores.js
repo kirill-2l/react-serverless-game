50e27f34994c19165b60d0e8fa0738a4a90bed8e
Kirill <petrovvkirill@gmail.com>
2020-07-15 14:13:58 +0300

    add save highscores serverless functions and make some refactoring

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const HighScores = () => <h1>HighScores</h1>;
+import { ScoresList, ScoreLI } from "../styled/HighScores";
+
+const HighScores = () => {
+  const [highScores, setHighScores] = useState([]);
+
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch("/.netlify/functions/getHighScores");
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadHighScores();
+  }, []);
+
+  return (
+    <div>
+      <h1>High Scores</h1>
+
+      <ScoresList>
+        {highScores.map((score) => (
+          <ScoreLI key={score.id}>{score.fields.name} — {score.fields.score}</ScoreLI>
+        ))}
+      </ScoresList>
+    </div>
+  );
+};
 
 export default HighScores;
